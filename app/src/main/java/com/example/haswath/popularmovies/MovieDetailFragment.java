99889a3d01c2920d07427c0869f8bfc6f1e17e2d
Harsha Aswath <haswath@elance-odesk.com>
2015-07-10 11:43:29 -0700

    initial version

---
+++ app/src/main/java/com/example/haswath/popularmovies/MovieDetailFragment.java
@@ -0,0 +1,80 @@
+package com.example.haswath.popularmovies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.haswath.popularmovies.dummy.DummyContent;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * A fragment representing a single Movie detail screen.
+ * This fragment is either contained in a {@link MovieListActivity}
+ * in two-pane mode (on tablets) or a {@link MovieDetailActivity}
+ * on handsets.
+ */
+public class MovieDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MovieDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        Log.i("detail", "loading image "+ mItem.content);
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            JSONObject jsonObject = mItem.content;
+            ImageView iv = ((ImageView) container.findViewById(R.id.imageView));
+            try {
+                String url = "http://image.tmdb.org/t/p/w185" + jsonObject.getString("poster_path");
+                Picasso.with(getActivity()).load(url).into(iv);
+                TextView tv = ((TextView) container.findViewById(R.id.textView));
+                tv.setText(jsonObject.getString("original_title"));
+                tv = ((TextView) container.findViewById(R.id.textView2));
+                tv.setText(jsonObject.getString("release_date").substring(0, 4));
+                tv = ((TextView) container.findViewById(R.id.tvOverview));
+                tv.setText(jsonObject.getString("overview"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/haswath/popularmovies/MovieListFragment.java
@@ -0,0 +1,326 @@
+package com.example.haswath.popularmovies;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import com.example.haswath.popularmovies.dummy.DummyContent;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A list fragment representing a list of Movies. This fragment
+ * also supports tablet devices by allowing list items to be given an
+ * 'activated' state upon selection. This helps indicate which item is
+ * currently being viewed in a {@link MovieDetailFragment}.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link Callbacks}
+ * interface.
+ */
+public class MovieListFragment extends ListFragment {
+
+    /**
+     * The serialization (saved instance state) Bundle key representing the
+     * activated item position. Only used on tablets.
+     */
+    private static final String STATE_ACTIVATED_POSITION = "activated_position";
+
+    /**
+     * The fragment's current callback object, which is notified of list item
+     * clicks.
+     */
+    private Callbacks mCallbacks = sDummyCallbacks;
+
+    /**
+     * The current activated item position. Only used on tablets.
+     */
+    private int mActivatedPosition = ListView.INVALID_POSITION;
+    private ArrayAdapter<DummyContent.DummyItem> adapter;
+    private WeakReference<FetchMoviesTask> asyncTaskWeakRef;
+    private static final String POSTER_PATH = "poster_path";
+
+
+    /**
+     * A callback interface that all activities containing this fragment must
+     * implement. This mechanism allows activities to be notified of item
+     * selections.
+     */
+    public interface Callbacks {
+        /**
+         * Callback for when an item has been selected.
+         */
+        public void onItemSelected(String id);
+    }
+
+    /**
+     * A dummy implementation of the {@link Callbacks} interface that does
+     * nothing. Used only when this fragment is not attached to an activity.
+     */
+    private static Callbacks sDummyCallbacks = new Callbacks() {
+        @Override
+        public void onItemSelected(String id) {
+        }
+    };
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MovieListFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        FetchMoviesTask weatherTask = new FetchMoviesTask(this, getActivity());
+        this.asyncTaskWeakRef = new WeakReference<FetchMoviesTask>(weatherTask );
+        weatherTask.execute();
+
+        // TODO: replace with a real list adapter.
+        List<DummyContent.DummyItem> items = new ArrayList<>();
+        this.adapter = new ArrayAdapter<DummyContent.DummyItem>(
+                getActivity(),
+                android.R.layout.simple_list_item_activated_1,
+                android.R.id.text1,
+                items);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Restore the previously serialized activated item position.
+        if (savedInstanceState != null
+                && savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
+            setActivatedPosition(savedInstanceState.getInt(STATE_ACTIVATED_POSITION));
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // Activities containing this fragment must implement its callbacks.
+        if (!(activity instanceof Callbacks)) {
+            throw new IllegalStateException("Activity must implement fragment's callbacks.");
+        }
+
+        mCallbacks = (Callbacks) activity;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        // Reset the active callbacks interface to the dummy implementation.
+        mCallbacks = sDummyCallbacks;
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View view, int position, long id) {
+        super.onListItemClick(listView, view, position, id);
+
+        // Notify the active callbacks interface (the activity, if the
+        // fragment is attached to one) that an item has been selected.
+        mCallbacks.onItemSelected(adapter.getItem(position).id);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mActivatedPosition != ListView.INVALID_POSITION) {
+            // Serialize and persist the activated item position.
+            outState.putInt(STATE_ACTIVATED_POSITION, mActivatedPosition);
+        }
+    }
+
+    /**
+     * Turns on activate-on-click mode. When this mode is on, list items will be
+     * given the 'activated' state when touched.
+     */
+    public void setActivateOnItemClick(boolean activateOnItemClick) {
+        // When setting CHOICE_MODE_SINGLE, ListView will automatically
+        // give items the 'activated' state when touched.
+        getListView().setChoiceMode(activateOnItemClick
+                ? ListView.CHOICE_MODE_SINGLE
+                : ListView.CHOICE_MODE_NONE);
+    }
+
+    private void setActivatedPosition(int position) {
+        if (position == ListView.INVALID_POSITION) {
+            getListView().setItemChecked(mActivatedPosition, false);
+        } else {
+            getListView().setItemChecked(position, true);
+        }
+
+        mActivatedPosition = position;
+    }
+
+    public class FetchMoviesTask extends AsyncTask<Void, Void, JSONObject[]> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+        private final WeakReference<MovieListFragment> fragmentWeakRef;
+        private Context context;
+
+        public FetchMoviesTask(MovieListFragment movieListFragment, Context context) {
+            this.fragmentWeakRef = new WeakReference<MovieListFragment>(movieListFragment);
+            this.context = context;
+        }
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private JSONObject[] getMovieDataFromJson(String moviesJsonStr)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "results";
+
+            JSONObject moviesJson = new JSONObject(moviesJsonStr);
+            JSONArray movies = moviesJson.getJSONArray(OWM_LIST);
+
+            JSONObject[] resultStrs = new JSONObject[movies.length()];
+            for(int i = 0; i < movies.length(); i++) {
+                // Get the JSON object representing the day
+                JSONObject movie = movies.getJSONObject(i);
+
+                resultStrs[i] = movie;
+            }
+            return resultStrs;
+
+        }
+        @Override
+        protected JSONObject[] doInBackground(Void... params) {
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL =
+                        "https://api.themoviedb.org/3/discover/movie?";
+                final String SORTBY_PARAM = "sort_by";
+                final String APIKEY_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(SORTBY_PARAM, "popularity.desc")
+                        .appendQueryParameter(APIKEY_PARAM, "e95feea469573da8277773d047559d1a")
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieDataFromJson(moviesJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject[] result) {
+            if (result != null) {
+                int i=0;
+                for(JSONObject jsonObject : result) {
+                    Log.i(LOG_TAG, "result " + jsonObject);
+//                    Picasso.with(context).load(url).into(imageView);
+                    String id = String.valueOf(i);
+                    try {
+                        String originalTitle = jsonObject.getString("original_title");
+                        DummyContent.DummyItem item = new DummyContent.DummyItem(id, jsonObject);
+                        adapter.add(item);
+                        DummyContent.ITEM_MAP.put(id, item);
+                        DummyContent.ITEMS.add(item);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    i++;
+                }
+                // New data is back from the server.  Hooray!
+            }
+        }
+    }
+}
